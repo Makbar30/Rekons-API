c899772e4b34a2de11632c8a5627860e2000cd8d
Akbarbar30 <mohammadakbar28@yahoo.co.id>
2020-01-22 11:53:21 +0700

    fix mysqlCon

--- models/mysqlCon.js
+++ models/mysqlCon.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 //- Connection configuration
 var con  = mysql.createPool({
   connectionLimit : 10,
-  host: "localhost",
+  host: "35.198.205.215",
   port: "3306",
   user: "root",
   password: "muslimpocket2019!@#",
